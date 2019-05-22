621bce8c7748e1bf48bfd5f31dff26317369e502
James Absalem Lintu <26407837+jamesabsalem@users.noreply.github.com>
2019-05-22 19:04:46 +0600

    first commit

---
+++ WordCapitalization/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace WordCapitalization
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToList();
+            var stringBuilder = new StringBuilder();
+            var firstLetter = Convert.ToChar(input[0].ToString().ToUpper());
+            input.RemoveAt(0);
+            stringBuilder.Append(firstLetter);
+            foreach (var i in input) stringBuilder.Append(i);
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
